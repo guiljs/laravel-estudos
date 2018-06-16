d9b1c7284117078436d2f09a4d7aae70be2b6c10
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-16 08:20:05 -0300

    Eloquent query all

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/tasks', function () {
 
     $tasks = DB::table('tasks')->get();
     $name = 'Guilherme';
     $teste = 'TESTE';
-     return view('tasks.index',compact('name','teste','tasks'));
+    return view('tasks.index', compact('name', 'teste', 'tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
 
     // dd($id); //Die and dump
-    $task = DB::table('tasks')->find($id);
+    // $tasks = DB::table('tasks')->find($id);
+    $tasks = App\Task::all();
 
-     return view('tasks.show',compact('task'));
+    return view('tasks.show', compact('tasks'));
 });
 
 Route::get('/about', function () {
